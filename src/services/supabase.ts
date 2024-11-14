923786b769bc6a86217b3fa0742c50ecc87f76f4
Developer <152862874+elijahnzeli1@users.noreply.github.com>
2024-11-14 17:31:24 +0300

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xpwylsnkjmldeojxbxbk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhwd3lsc25ram1sZGVvanhieGJrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjQ5NjU1NywiZXhwIjoyMDQyMDcyNTU3fQ.ynvuosXGY8ns7ZXLrUinuNKtJ9QdPmVcNThgjzaWY-c';
+const supabaseUrl = 'YOUR_BASEURL';
+const supabaseKey = 'YOUR_API_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
